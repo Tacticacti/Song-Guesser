14723f9afb89c10cd1a657d2128bad0c83b9d35f
Edward Oh Jang Hun <Tactikalkacti@gmail.com>
2026-05-08 18:46:25 +0200

    Handled edge case of maximum number of songs in results

--- api_fetcher.py
+++ api_fetcher.py
@@ -4,7 +4,8 @@ import random
 def fetch_metadata(url, params):
     response = requests.get(url, params)
     # print(response.json()['results'][0])
-    data = response.json()['results'][random.randint(0, 50)]
+    results = response.json()['results']
+    data = results[random.randint(0, len(results))]
     artist_name = data['artistName']
     track_name = data['trackName']
     release_date = data['releaseDate']
