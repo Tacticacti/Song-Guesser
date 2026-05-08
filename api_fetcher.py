f48042c1c159dddce0fef101cb24cd23ae576c2d
Edward Oh Jang Hun <Tactikalkacti@gmail.com>
2026-05-08 18:41:03 +0200

    Changed fetch function so that it gets random songs from the top 50 songs of that artist

--- api_fetcher.py
+++ api_fetcher.py
@@ -1,11 +1,12 @@
 import requests
+import random
 
-def fetch_metadata(url):
-    response = requests.get(url)
+def fetch_metadata(url, params):
+    response = requests.get(url, params)
     # print(response.json()['results'][0])
-    data = response.json()
-    artist_name = data['results'][0]['artistName']
-    track_name = data['results'][0]['trackName']
-    release_date = data['results'][0]['releaseDate']
-    preview_url = data['results'][0]['previewUrl']
+    data = response.json()['results'][random.randint(0, 50)]
+    artist_name = data['artistName']
+    track_name = data['trackName']
+    release_date = data['releaseDate']
+    preview_url = data['previewUrl']
     return artist_name, track_name, release_date, preview_url
