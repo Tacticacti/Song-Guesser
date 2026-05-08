43f3bc51c1ec315030c0d647dcebe759d1a66c43
Edward Oh Jang Hun <Tactikalkacti@gmail.com>
2026-05-08 22:14:21 +0200

    Refactored and added error check when artist doesn't exist

--- main.py
+++ main.py
@@ -5,12 +5,32 @@ import random
 MAX_STRIKES = 3
 EXTERNAL_URL = 'https://itunes.apple.com/search'
 
+def evaluate_guess(fetched_artist_name, fetched_track_name, correct_year, guess_val, strikes, score):
+    if guess_val == correct_year:
+        score += 1
+        print(f"You got it right!")
+    else:
+        strikes += 1
+        year_difference = abs(correct_year - guess_val)
+        if year_difference < 5:
+            print("So Close!")
+        elif year_difference < 10:
+            print("Close! But not close enough!")
+        else:
+            print("Way off!")
+    print(f"This song was {fetched_track_name} by {fetched_artist_name}!")
+
 def start_game(artist_pool, strikes, score):
     while strikes < MAX_STRIKES:
         print(f"\n--- score: {score} | strikes: {strikes}/{MAX_STRIKES} ---")
         artist = random.choice(artist_pool)
-        params = {"term": artist, "media": "music", "entity": "song"}
-        fetched_artist_name, fetched_track_name, fetched_release_date, fetched_preview_url = fetch_metadata(EXTERNAL_URL, params)
+        params = {"term": artist, "media": "music", "entity": "song", "attribute": "artistTerm"}
+        try:
+            fetched_artist_name, fetched_track_name, fetched_release_date, fetched_preview_url = fetch_metadata(EXTERNAL_URL, params)
+        except ValueError:
+            print(f"The artist {params['term']} was not found!")
+            print('Skipping artist...')
+            continue
         correct_year = int(fetched_release_date.split("-")[0])
 
         player_instance = play_audio(fetched_preview_url)
@@ -25,19 +45,7 @@ def start_game(artist_pool, strikes, score):
             except ValueError:
                 print("Please enter a number between 0 and 2026!")
 
-        if guess_val == correct_year:
-            score += 1
-            print(f"You got it right!")
-        else:
-            strikes += 1
-            year_difference = abs(correct_year - guess_val)
-            if year_difference < 5:
-                print("So Close!")
-            elif year_difference < 10:
-                print("Close! But not close enough!")
-            else:
-                print("Way off!")
-        print(f"This song was {fetched_track_name} by {fetched_artist_name}!")
+        evaluate_guess(fetched_artist_name, fetched_track_name, correct_year, guess_val, strikes, score)
 
 def populate_artist_pool():
     with open('artists.txt', 'r') as file:
