e9f3fc377298a6ff717c4c191a42e511ddf9ba1d
Edward Oh Jang Hun <Tactikalkacti@gmail.com>
2026-05-10 21:31:33 +0200

    Extracted getting random index functionality from fetch_metadata function to make it usable in testing

--- api_fetcher.py
+++ api_fetcher.py
@@ -6,9 +6,12 @@ def fetch_metadata(url, params):
     results = response.json()['results']
     if len(results) == 0:
         raise ValueError()
-    data = results[random.randint(0, len(results) - 1)]
+    data = results[get_random_index(results)]
     artist_name = data['artistName']
     track_name = data['trackName']
     release_date = data['releaseDate']
     preview_url = data['previewUrl']
     return artist_name, track_name, release_date, preview_url
+
+def get_random_index(results):
+    return random.randint(0, len(results) - 1)
